a43a2c20c8ce849135e759a51126597c13032e8d
Your-Full-Name <tarunreddy099@gmail.com>
2019-10-02 12:42:21 +0530

    Initial commit

---
+++ app/src/main/java/com/example/np/NectarHome.java
@@ -0,0 +1,14 @@
+package com.example.np;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NectarHome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nectar_home);
+    }
+}
